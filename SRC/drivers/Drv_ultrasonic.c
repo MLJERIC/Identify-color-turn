e071f89659ef9c774a87c156c651c6cc7064a7dc
hekdh <1059493472@qq.com>
2021-05-26 21:31:56 +0800

    有一个文件没保存，重新上传一下

--- SRC/drivers/Drv_ultrasonic.c
+++ SRC/drivers/Drv_ultrasonic.c
@@ -85,64 +85,63 @@ void UltraSonic_Byte_Get(u8 bytedata)
 	}
 }
 
-void ct_state_task()
-{
-	static u8 state=0;
-	static u8 state_loss=0;
-	if(state==0)
-	{
-		if(opmv.offline==0)//视野中是否出现啊红色杆子
-		{
-			Program_Ctrl_User_Set_YAWdps(-4*opmv.cb.pos_x);//根据识别，控制杆子在视野中央
-			if(ABS(opmv.cb.pos_x)<20)//如果在视野中央范围内
-			{
-				state++;		//进入下一个阶段
-			}		
-		}
-		else
-		{		
-			Program_Ctrl_User_Set_YAWdps(10);
-		}		
-	}
-	else if(state ==1)//进入阶段2，距离控制
-	{
-		if(flag.offline)
-		{
-		if(flag.distance>350||flag.distance<250)//不在可转动范围内时
-		{
-			pc_user.vel_cmps_set_h[0] = 0.1*(flag.distance-50);		//控制距离
-		}
-		else 
-		{
-			state++;//当在可转动范围时，进入下一个阶段
-		}
-		}
-		if(opmv.offline)//如果此时突然opmv掉线或超声波掉线，回到状态1
-		{
-			state =0;		
-		}		
-	}
-	else if(state ==2)
-	{
-		
-			pc_user.vel_cmps_set_h[1]=10;//传感器都正常工作时进入阶段3，横向移动
-			if(opmv.offline)//移动到opmv失去视觉时，回到第一阶段
-			{
-				state=0;
-				pc_user.vel_cmps_set_h[1]=0;
-			}	
-	}
+//void ct_state_task()
+//{
+//	static u8 state=0;
+//	static u8 state_loss=0;
+//	if(state==0)
+//	{
+//		if(opmv.offline==0)//视野中是否出现啊红色杆子
+//		{
+//			Program_Ctrl_User_Set_YAWdps(-4*opmv.cb.pos_x);//根据识别，控制杆子在视野中央
+//			if(ABS(opmv.cb.pos_x)<20)//如果在视野中央范围内
+//			{
+//				state++;		//进入下一个阶段
+//			}		
+//		}
+//		else
+//		{		
+//			Program_Ctrl_User_Set_YAWdps(10);
+//		}		
+//	}
+//	else if(state ==1)//进入阶段2，距离控制
+//	{
+//		if(flag.offline)
+//		{
+//		if(flag.distance>350||flag.distance<250)//不在可转动范围内时
+//		{
+//			pc_user.vel_cmps_set_h[0] = 0.1*(flag.distance-350);		//控制距离
+//		}
+//		else 
+//		{
+//			state++;//当在可转动范围时，进入下一个阶段
+//		}
+//		}
+//		if(opmv.offline)//如果此时突然opmv掉线或超声波掉线，回到状态1
+//		{
+//			state =0;		
+//		}		
+//	}
+//	else if(state ==2)
+//	{
+//		
+//			pc_user.vel_cmps_set_h[1]=10;//传感器都正常工作时进入阶段3，横向移动
+//			if(opmv.offline)//移动到opmv失去视觉时，回到第一阶段
+//			{
+//				state=0;
+//				pc_user.vel_cmps_set_h[1]=0;
+//			}	
+//	}
 
-}
+//}
 
 u8 ct_state_task1()//yaw轴控制
 {
 	if(opmv.offline==0)//杆子出现在视野中
 	{	
-		if(ABS(opmv.cb.pos_x)>10)//杆子不在视野中央
+		if(ABS(opmv.cb.pos_x)>20)//杆子不在视野中央
 		{
 			Program_Ctrl_User_Set_YAWdps(-1*opmv.cb.pos_x);
-			
 			return 1;
 		}
 		else
@@ -161,11 +160,11 @@ u8 ct_state_task1()//yaw轴控制
 
 void ct_state_task2()//前后移动控制
 {
-	if(ct_state_task1()==2)//视野正常且在正中央
+	if(ct_state_task1()==1)//视野正常且在正中央
 	{		
 		if(flag.offline==0)//超声波有识别
 		{
-			pc_user.vel_cmps_set_h[0] = 0.1*(flag.distance-550);		//控制距离
+			pc_user.vel_cmps_set_h[0] = 0.1*(flag.distance-350);		//控制距离
 		}
 		else
 		{
@@ -176,9 +175,9 @@ void ct_state_task2()//前后移动控制
 
 void ct_state_task3()
 {
-	if(ct_state_task1()==2)
+	if(ct_state_task1()==1)
 	{
-		pc_user.vel_cmps_set_h[1]=-5;     //横向移动
+		pc_user.vel_cmps_set_h[1]=5;     //横向移动
 	}
 	else
 	{
